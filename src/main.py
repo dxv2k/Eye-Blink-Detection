a8031e246cd24bc7581e66cf5e4844b44583b22b
knightV <9569076+dxv2k@users.noreply.github.com>
2021-04-12 00:50:19 +0700

    working on predictor

--- src/main.py
+++ src/main.py
@@ -38,10 +38,89 @@ args = vars(ap.parse_args())
 print("[INFO] loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
 # predictor = dlib.shape_predictor(args["shape_predictor"])
-
-
-
 datFile = r"tests/shape_predictor_68_face_landmarks (1).dat"
-
 predictor = dlib.shape_predictor(datFile)
 
+
+
+
+# start the video stream thread
+print("[INFO] starting video stream thread...")
+vs = FileVideoStream(args["video"]).start()
+fileStream = True
+# vs = VideoStream(src=0).start()
+# vs = VideoStream(usePiCamera=True).start()
+# fileStream = False
+time.sleep(1.0)
+
+
+
+# loop over frames from the video stream
+while True:
+	# if this is a file video stream, then we need to check if
+	# there any more frames left in the buffer to process
+	if fileStream and not vs.more():
+		break
+	# grab the frame from the threaded video file stream, resize
+	# it, and convert it to grayscale
+	# channels)
+	frame = vs.read()
+	# frame = imutils.resize(frame, width=450) # ERROR here
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# detect faces in the grayscale frame
+	rects = detector(gray, 0)
+
+	# loop over the face detections
+	for rect in rects:
+		# determine the facial landmarks for the face region, then
+		# convert the facial landmark (x, y)-coordinates to a NumPy
+		# array
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
+		# extract the left and right eye coordinates, then use the
+		# coordinates to compute the eye aspect ratio for both eyes
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[rStart:rEnd]
+		leftEAR = eye_aspect_ratio(leftEye)
+		rightEAR = eye_aspect_ratio(rightEye)
+		# average the eye aspect ratio together for both eyes
+		ear = (leftEAR + rightEAR) / 2.0
+
+		# compute the convex hull for the left and right eye, then
+		# visualize each of the eyes
+		leftEyeHull = cv2.convexHull(leftEye)
+		rightEyeHull = cv2.convexHull(rightEye)
+		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+		# check to see if the eye aspect ratio is below the blink
+		# threshold, and if so, increment the blink frame counter
+		if ear < EYE_AR_THRESH:
+			COUNTER += 1
+		# otherwise, the eye aspect ratio is not below the blink
+		# threshold
+		else:
+			# if the eyes were closed for a sufficient number of
+			# then increment the total number of blinks
+			if COUNTER >= EYE_AR_CONSEC_FRAMES:
+				TOTAL += 1
+			# reset the eye frame counter
+			COUNTER = 0	
+		# draw the total number of blinks on the frame along with
+		# the computed eye aspect ratio for the frame
+		cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+ 
+	# show the frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+ 
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
--- tests/predictor.py
+++ tests/predictor.py
@@ -1,15 +1,32 @@
 import cv2 
 import dlib
 
+
+
+from imutils import face_utils
+
 # Passing '0` on windows, `-1` on linux
 cap = cv2.VideoCapture(0)
 
 hog_face_detector = dlib.get_frontal_face_detector()
 
-
+# Pretrained face detector based on Historam of Oriented Gradients + Linear SVM
 # datFile = './shape_predictor_68_face_landmarks.dat'
 datFile = r"tests/shape_predictor_68_face_landmarks (1).dat"
-dlib_facelandmark = dlib.shape_predictor(datFile)
+# dlib_facelandmark = dlib.shape_predictor(datFile)
+predictor = dlib.shape_predictor(datFile)
+
+
+# grab the indexes of the facial landmarks for the left and
+# right eye, respectively
+# Ref: https://www.pyimagesearch.com/2017/04/10/detect-eyes-nose-lips-jaw-dlib-opencv-python/
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"] #42,48 
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"] # 36, 42
+
+# print(lStart,lEnd)
+# print('-----------------')
+# print(rStart,rEnd)
+
 
 while True:  
     ret, frame = cap.read() 
@@ -18,13 +35,19 @@ while True:
     faces = hog_face_detector(gray)
     for face in faces:
 
-        face_landmarks = dlib_facelandmark(gray, face)
-
+        # Draw key chin keypoints   
+        # face_landmarks = dlib_facelandmark(gray, face)
+        face_landmarks = predictor(gray, face)
         for n in range(0, 16):
             x = face_landmarks.part(n).x
             y = face_landmarks.part(n).y
             cv2.circle(frame, (x, y), 1, (0, 255, 255), 1)
 
+        # TODO: detect eye and draw bb
+        # convert face_landmarks to np 
+        shape = face_utils.shape_to_np(face_landmarks)
+        # left
+        # print(shape)
 
     cv2.imshow("Face Landmarks", frame)
 
